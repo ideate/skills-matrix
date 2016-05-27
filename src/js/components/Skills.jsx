66ac4be5b27284ba29a7551448e4df075d78e8dc
comptode <comptode@indiana.edu>
2016-05-27 15:07:12 -0400

    Grid branch (#11)
    * Added very rudimentary ag-grid set up on skills
    
    * Added actual skills to the ag-grid and added some theme/css
    
    * Updated ag-grid to use ag-material theme
    
    * Added material-ui theme with some fontawesome icons to ag-grid
    
    * Added checkboxes to grids
    
    * Added ag-grid to every page
    
    * Fixes to coding style

--- src/js/components/Skills.jsx
+++ src/js/components/Skills.jsx
@@ -1,46 +1,107 @@
+import {AgGridReact} from 'ag-grid-react'
 import {browserHistory} from 'react-router'
 import {connect} from 'react-redux'
-import FlatButton from 'material-ui/FlatButton'
 import {main} from '../styles/common'
 import RaisedButton from 'material-ui/RaisedButton'
 import {skillsRead} from '../modules/async/skills-read'
 import React, {Component, PropTypes} from 'react'
-import {Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn} from 'material-ui/Table'
 import {Toolbar, ToolbarGroup, ToolbarTitle} from 'material-ui/Toolbar'
 
 class Skills extends Component {
+  
+  constructor () {
+    super()
+
+    this.state = {
+      editIcon: '<i class="fa fa-pencil-square-o fa-lg"/>',
+      icons: {
+        columnRemoveFromGroup: '<i class="fa fa-remove"/>',
+        filter: '<i class="fa fa-filter"/>',
+        menu: '<i class="fa fa-bars"/>',
+        sortAscending: '<i class="fa fa-sort-alpha-asc"/>',
+        sortDescending: '<i class="fa fa-sort-alpha-desc"/>'
+      }
+    }
+  }
+
   static propTypes = {
     dispatch: PropTypes.func.isRequired,
     skillsState: PropTypes.object.isRequired
   }
-
+  
+  componentDidUpdate () {
+    this.handleResize(this.refs.grid)
+  }
+  
   componentWillMount () {
     const {dispatch} = this.props
 
     dispatch(skillsRead())
   }
-
+  
+  handleResize (grid) {
+    grid.api.sizeColumnsToFit()
+  }
+  
+  onCellClicked (event) {
+    if (event.value === this.state.editIcon) {
+      browserHistory.push('/skills/' + event.data.id)
+    }
+  }
+  
+  onGridReady (grid) {
+    grid.api.sizeColumnsToFit()
+  }
+  
   renderSkills () {
     const {skillsState} = this.props
+    const editIcon = this.state.editIcon
 
     if (skillsState && skillsState.data && skillsState.data.length) {
+      const columnDefs = [
+        {
+          headerName: '',
+          checkboxSelection: true,
+          suppressMenu: true,
+          suppressMovable: true,
+          suppressResize: true,
+          suppressSorting: true,
+          width: 15
+        },
+        {headerName: 'Skill', field: 'title', cellStyle: {color: '#FF4081'}},
+        {headerName: 'Description', field: 'description'},
+        {
+          headerName: 'Edit',
+          field: 'edit',
+          suppressMenu: true,
+          suppressMovable: true,
+          suppressResize: true,
+          suppressSorting: true,
+          width: 20
+        }
+      ]
+      const rowData = []
+
+      skillsState.data.map(function (skill) {
+        rowData.push({id: skill._id, title: skill.title, description: skill.description, edit: editIcon})
+      })
+      
       return (
-        skillsState.data.map(function (skill) {
-          return (
-            <TableRow key={skill._id}>
-              <TableRowColumn>
-                <FlatButton
-                  label={skill.title}
-                  secondary={true}
-                  onTouchTap={() => {
-                    browserHistory.push(`/skills/${skill._id}`)
-                  }}
-                />
-              </TableRowColumn>
-              <TableRowColumn>{skill.description}</TableRowColumn>
-            </TableRow>
-          )
-        })
+        <AgGridReact
+          autoResize='true'
+          columnDefs={columnDefs}
+          enableColResize='true'
+          enableFilter='true'
+          enableSorting='true'
+          headerHeight='48'
+          icons={this.state.icons}
+          ref='grid'
+          rowData={rowData}
+          rowHeight='48'
+          rowSelection='multiple'
+          onCellClicked={this.onCellClicked.bind(this)}
+          onGridReady={this.onGridReady.bind(this)}
+        />
       )
     }
   }
@@ -61,26 +122,9 @@ class Skills extends Component {
           </ToolbarGroup>
         </Toolbar>
         <main style={main}>
-          <Table
-            multiSelectable={false}
-            selectable={false}
-          >
-            <TableHeader
-              adjustForCheckbox={false}
-              displaySelectAll={false}
-              enableSelectAll={false}
-            >
-              <TableRow>
-                <TableHeaderColumn>Skill</TableHeaderColumn>
-                <TableHeaderColumn>Description</TableHeaderColumn>
-              </TableRow>
-            </TableHeader>
-            <TableBody
-              displayRowCheckbox={false}
-            >
-              {this.renderSkills()}
-            </TableBody>
-          </Table>
+          <div className='ag-material'>
+           {this.renderSkills()}
+           </div>
         </main>
       </div>
     )
--- src/js/components/Strategies.jsx
+++ src/js/components/Strategies.jsx
@@ -1,46 +1,107 @@
+import {AgGridReact} from 'ag-grid-react'
 import {browserHistory} from 'react-router'
 import {connect} from 'react-redux'
-import FlatButton from 'material-ui/FlatButton'
 import {main} from '../styles/common'
 import RaisedButton from 'material-ui/RaisedButton'
 import {strategiesRead} from '../modules/async/strategies-read'
 import React, {Component, PropTypes} from 'react'
-import {Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn} from 'material-ui/Table'
 import {Toolbar, ToolbarGroup, ToolbarTitle} from 'material-ui/Toolbar'
 
 class Strategies extends Component {
+  
+  constructor () {
+    super()
+
+    this.state = {
+      editIcon: '<i class="fa fa-pencil-square-o fa-lg"/>',
+      icons: {
+        columnRemoveFromGroup: '<i class="fa fa-remove"/>',
+        filter: '<i class="fa fa-filter"/>',
+        menu: '<i class="fa fa-bars"/>',
+        sortAscending: '<i class="fa fa-sort-alpha-asc"/>',
+        sortDescending: '<i class="fa fa-sort-alpha-desc"/>'
+      }
+    }
+  }
+
   static propTypes = {
     dispatch: PropTypes.func.isRequired,
     strategiesState: PropTypes.object.isRequired
   }
-
+  
+  componentDidUpdate () {
+    this.handleResize(this.refs.grid)
+  }
+  
   componentWillMount () {
     const {dispatch} = this.props
 
     dispatch(strategiesRead())
   }
-
+  
+  handleResize (grid) {
+    grid.api.sizeColumnsToFit()
+  }
+  
+  onCellClicked (event) {
+    if (event.value === this.state.editIcon) {
+      browserHistory.push('/strategies/' + event.data.id)
+    }
+  }
+  
+  onGridReady (grid) {
+    grid.api.sizeColumnsToFit()
+  }
+  
   renderStrategies () {
     const {strategiesState} = this.props
+    const editIcon = this.state.editIcon
 
     if (strategiesState && strategiesState.data && strategiesState.data.length) {
+      const columnDefs = [
+        {
+          headerName: '',
+          checkboxSelection: true,
+          suppressMenu: true,
+          suppressMovable: true,
+          suppressResize: true,
+          suppressSorting: true,
+          width: 15
+        },
+        {headerName: 'Strategy', field: 'title', cellStyle: {color: '#FF4081'}},
+        {headerName: 'Description', field: 'description'},
+        {
+          headerName: 'Edit',
+          field: 'edit',
+          suppressMenu: true,
+          suppressMovable: true,
+          suppressResize: true,
+          suppressSorting: true,
+          width: 20
+        }
+      ]
+      const rowData = []
+
+      strategiesState.data.map(function (strategy) {
+        rowData.push({id: strategy._id, title: strategy.title, description: strategy.description, edit: editIcon})
+      })
+      
       return (
-        strategiesState.data.map(function (strategy) {
-          return (
-            <TableRow key={strategy._id}>
-              <TableRowColumn>
-                <FlatButton
-                  label={strategy.title}
-                  secondary={true}
-                  onTouchTap={() => {
-                    browserHistory.push(`/strategies/${strategy._id}`)
-                  }}
-                />
-              </TableRowColumn>
-              <TableRowColumn>{strategy.description}</TableRowColumn>
-            </TableRow>
-          )
-        })
+        <AgGridReact
+          autoResize='true'
+          columnDefs={columnDefs}
+          enableColResize='true'
+          enableFilter='true'
+          enableSorting='true'
+          headerHeight='48'
+          icons={this.state.icons}
+          ref='grid'
+          rowData={rowData}
+          rowHeight='48'
+          rowSelection='multiple'
+          onCellClicked={this.onCellClicked.bind(this)}
+          onGridReady={this.onGridReady.bind(this)}
+        />
       )
     }
   }
@@ -61,26 +122,9 @@ class Strategies extends Component {
           </ToolbarGroup>
         </Toolbar>
         <main style={main}>
-          <Table
-            multiSelectable={false}
-            selectable={false}
-          >
-            <TableHeader
-              adjustForCheckbox={false}
-              displaySelectAll={false}
-              enableSelectAll={false}
-            >
-              <TableRow>
-                <TableHeaderColumn>Strategy</TableHeaderColumn>
-                <TableHeaderColumn>Description</TableHeaderColumn>
-              </TableRow>
-            </TableHeader>
-            <TableBody
-              displayRowCheckbox={false}
-            >
-              {this.renderStrategies()}
-            </TableBody>
-          </Table>
+          <div className='ag-material'>
+           {this.renderStrategies()}
+           </div>
         </main>
       </div>
     )
