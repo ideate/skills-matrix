d4ea33263b03b41247a917aaa4bf4ba54dc9d1e1
David <cafedavid@users.noreply.github.com>
2016-06-30 17:04:04 -0400

    code cleanup (#84)

--- src/js/components/Wrapper.jsx
+++ src/js/components/Wrapper.jsx
@@ -1,24 +1,3 @@
-// import {large} from '../styles/media-queries'
-// import Radium from 'radium'
-// import React, {PropTypes} from 'react'
-
-// const style = {
-//   padding: '0 0 0 0px',
-//   [large]: {
-//     padding: '0 0 0 0px'
-//   }
-// }
-
-// export const Wrapper = Radium(({children}) => (
-//   <div style={style}>
-//     {children}
-//   </div>
-// ))
-
-// Wrapper.propTypes = {
-//   children: PropTypes.node
-// }
-
 import Radium from 'radium'
 import React, {PropTypes} from 'react'
 
